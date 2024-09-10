f4705463fdac12691eda981642dfb09ab71d1877
putrid slurry <547366@student.saxion.nl>
2024-09-10 12:54:54 +0200

    control wheel torque

--- Assets/Sandboxes/Caspar/Car/CarController.cs
+++ Assets/Sandboxes/Caspar/Car/CarController.cs
@@ -1,20 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    [SerializeField] WheelCollider[] frontWheelColliders = new WheelCollider[2];
+    [Serializable] 
+    public struct Wheel
+    {
+        public WheelCollider collider;
+        public Transform renderer;
+        public bool frontWheel;
+    }
+    [SerializeField] Wheel[] Wheels = new Wheel[4];
+    [SerializeField] AnimationCurve EngineStrengthAtSpeed;
+    [SerializeField] float MaximumSpeed = 30;
+    [SerializeField] float EngineStrength = 50;
+    [SerializeField] float BrakeStrength = 150;
+
+    Rigidbody carRB;
+    float targetSpeed = 0;
     private void Start()
     {
-        GetComponent<SteeringManager>().SteeringAngleChanged += OnSteer;
-        if (frontWheelColliders[0] == null || frontWheelColliders[1] == null)
-            Debug.LogWarning("front wheels were not properly set");
+        var controlsHandler = GetComponent<CarControlsHandler>();
+
+        carRB = GetComponent<Rigidbody>();
+
+        if(carRB == null || EngineStrengthAtSpeed == null || controlsHandler == null)
+        {
+            Debug.LogWarning("CarController was not set up properly - some essential values were null.");
+            Destroy(this);
+        }
+
+        controlsHandler.SteeringAngleChanged += OnSteer;
+        controlsHandler.CarSpeedChanged += OnGas;
     }
 
     void OnSteer(float newAngle)
     {
-        frontWheelColliders[0].steerAngle = newAngle*30f;
-        frontWheelColliders[1].steerAngle = newAngle*30f;
+        //set the angle of the wheel's colliders
+        foreach(Wheel wheel in Wheels)
+            if(wheel.frontWheel)
+                wheel.collider.steerAngle = newAngle*30;
+    }
+
+    void OnGas(float gas)
+    {
+        gas *= Time.deltaTime;
+        
+        //targetSpeed is between 0-1, defining the speed at which the car is trying to cruise
+        targetSpeed += gas;
+        targetSpeed = Mathf.Clamp01(targetSpeed);
+
+        //if the car is already near maximum speed, power down the engine
+        float currentSpeed = Vector3.Dot(carRB.velocity,transform.forward) / MaximumSpeed;
+        float engineStrength = EngineStrengthAtSpeed.Evaluate(currentSpeed);
+
+        float torque = engineStrength*targetSpeed;
+        foreach(Wheel wheel in Wheels)
+        {
+            //if gas is hit or neutral, dont brake but gas
+            if(gas >= 0)
+            {
+                wheel.collider.motorTorque = torque*EngineStrength;
+                wheel.collider.brakeTorque = 0;
+            }
+            else
+            {
+                wheel.collider.motorTorque = 0;
+                wheel.collider.brakeTorque = BrakeStrength;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            int sl = 9;
+        }
     }
 }
--- Assets/Sandboxes/Caspar/Car/CarControlsHandler.cs
+++ Assets/Sandboxes/Caspar/Car/CarControlsHandler.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SteeringManager : MonoBehaviour
+public class CarControlsHandler : MonoBehaviour
 {
     public event Action<float> SteeringAngleChanged;
+    public event Action<float> CarSpeedChanged;
 
+    //temporary controls - both actions are expected to be between -1, 1 at all times
     private void Update()
     {
         float steeringAngle = Input.GetAxis("Horizontal");
         SteeringAngleChanged(steeringAngle);
+
+        float gas = Input.GetAxis("Vertical");
+        CarSpeedChanged(gas);
     }
 }
