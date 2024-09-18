e7a731a8f2c2769005729811e664feaa5f179f2d
Birka <birkavidit@gmail.com>
2024-09-18 17:35:51 +0200

    drawer with items working but not perfect

---
+++ Assets/Sandboxes/Stefan/GloveBox.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+[RequireComponent(typeof(GrabbableItem))]
+public class GloveBox : MonoBehaviour
+{
+    [SerializeField] Transform _renderer;
+    [SerializeField] Transform _openRot;
+    [SerializeField] Transform _closeRot;
+
+    [SerializeField] Transform _spawnSpots;
+
+    [SerializeField] GameObject[] _itemsRandomPool;
+
+    GrabbableItem _grabbableItem;
+
+    bool _isOpen;
+
+    void Awake()
+    {
+        _renderer.parent = null;
+        _renderer.parent = _closeRot;
+
+        _grabbableItem = GetComponent<GrabbableItem>();
+        _grabbableItem.onPlayerInteract.AddListener(controller =>
+        {
+            _isOpen = !_isOpen;
+            if(_isOpen)
+            {
+                _renderer.parent = null;
+                _renderer.parent = _openRot;
+                _renderer.localRotation = Quaternion.identity;
+
+                foreach (Transform spot in _spawnSpots)
+                {
+                    if(spot.childCount > 0)
+                    Destroy(spot.GetChild(0).gameObject);
+                    var item = Instantiate(_itemsRandomPool[Random.Range(0, _itemsRandomPool.Length)], spot);
+                }
+            }
+            else
+            {
+                _renderer.parent = null;
+                _renderer.parent = _closeRot;
+                _renderer.localRotation = Quaternion.identity;
+
+            }
+        });
+    }
+
+
+}
