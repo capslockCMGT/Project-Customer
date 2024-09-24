4d59cfc730a948467945984349988d16a0fb85b4
Eliotosaure <elioteosaure.veroteosaure@gmail.com>
2024-09-24 13:56:51 +0200

    Tutorial scene and fading screen

--- Assets/Global Scripts/GameManager.cs
+++ Assets/Global Scripts/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour
     readonly List<PlayerInput> _playerInputs = new(2);
     public static GameManager Instance { get; private set; }
     [field: SerializeField] public GameObject PlayerCar { get; private set; }
-    
+
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -32,9 +32,13 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         if (_grid != null)
+        {
             OnMapGenerated();
+            PlayerCar.GetComponentInChildren<NavigationDisplayRenderer>().Init(_grid);
+        }
 
-        PlayerCar.GetComponentInChildren<NavigationDisplayRenderer>().Init(_grid);
+
+
         StartCoroutine(Utils.DoFadeOut(_fadeScreen, _fadeScreenTime, 0));
 
     }
@@ -77,6 +81,7 @@ public class GameManager : MonoBehaviour
 
     public void FinishLevel()
     {
+        _fadeScreen.gameObject.SetActive(true);
         StartCoroutine(Utils.DoFadeIn(_fadeScreen, _fadeScreenTime, 0, () => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1)));
 
     }
