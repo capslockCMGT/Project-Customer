d612d6485097c2c6728c99624904cc4cd9a503b2
putrid slurry <547366@student.saxion.nl>
2024-09-26 12:54:24 +0200

    what if the exit button worked

--- Assets/Sandboxes/Caspar/PauseMenu.cs
+++ Assets/Sandboxes/Caspar/PauseMenu.cs
@@ -14,9 +14,15 @@ public class PauseMenu : MonoBehaviour
         if (_menu == null)
             _menu = Menu;
     }
+    public void ForcePauseOff()
+    {
+        _menu?.SetActive(false);
+        Time.timeScale = _paused ? 0 : 1;
+        _paused = false;
+    }
     public void TogglePause()
     {
-        if (GameManager.IsGameOver) return;
+        if (GameManager.IsGameOver && !_paused) return;
         _paused = !_paused;
         Time.timeScale = _paused ? 0 : 1;
         _menu?.SetActive(_paused);
